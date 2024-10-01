1d8688f95d4969595bb60dff3d5578b3290b4a7c
Tyler Lum <tylergwlum@gmail.com>
2024-10-01 16:12:47 -0700

    Update fabric world to be cleaner and better match the real world with explicit sfaety buffer

--- fabric_world.py
+++ fabric_world.py
@@ -3,7 +3,7 @@ from scipy.spatial.transform import Rotation as R
 
 
 def transform_str_to_T(transform_str: str) -> np.ndarray:
-    # Go from string like f"0 0 {HEIGHT} 0 0 0 1"  # x y z qx qy qz qw
+    # Go from string like f"0 0 {MAX_HEIGHT} 0 0 0 1"  # x y z qx qy qz qw
     # To 4x4 matrix
     xyz_qxyzw = np.array([float(x) for x in transform_str.split()])
     from scipy.spatial.transform import Rotation as R
@@ -16,7 +16,7 @@ def transform_str_to_T(transform_str: str) -> np.ndarray:
 
 
 def T_to_transform_str(T: np.ndarray) -> str:
-    # Go from 4x4 matrix to string like f"0 0 {HEIGHT} 0 0 0 1"  # x y z qx qy qz qw
+    # Go from 4x4 matrix to string like f"0 0 {MAX_HEIGHT} 0 0 0 1"  # x y z qx qy qz qw
     r = R.from_matrix(T[:3, :3])
     xyz_qxyzw = np.zeros(7)
     xyz_qxyzw[:3] = T[:3, 3]
@@ -24,8 +24,8 @@ def T_to_transform_str(T: np.ndarray) -> str:
     return " ".join([str(x) for x in xyz_qxyzw])
 
 
-THICKNESS = 0.02
-HEIGHT = 1
+THICKNESS = 0.05
+MAX_HEIGHT = 1
 
 # Conservative
 # TABLE_X_LEN = 0.5
@@ -39,26 +39,26 @@ world_dict_table_frame = {
     "right_wall": {
         "env_index": "all",
         "type": "box",
-        "scaling": f"{2*TABLE_X_LEN} {THICKNESS} {HEIGHT}",
-        "transform": f"0 {0.5*TABLE_Y_LEN} {0.5 * HEIGHT} 0 0 0 1",  # x y z qx qy qz qw
+        "scaling": f"{2*TABLE_X_LEN} {THICKNESS} {MAX_HEIGHT}",
+        "transform": f"0 {0.5*TABLE_Y_LEN} {0.5 * MAX_HEIGHT} 0 0 0 1",  # x y z qx qy qz qw
     },
     "left_wall": {
         "env_index": "all",
         "type": "box",
-        "scaling": f"{2*TABLE_X_LEN} {THICKNESS} {HEIGHT}",
-        "transform": f"0 {-0.5*TABLE_Y_LEN} {0.5 * HEIGHT} 0 0 0 1",  # x y z qx qy qz qw
+        "scaling": f"{2*TABLE_X_LEN} {THICKNESS} {MAX_HEIGHT}",
+        "transform": f"0 {-0.5*TABLE_Y_LEN} {0.5 * MAX_HEIGHT} 0 0 0 1",  # x y z qx qy qz qw
     },
     "back_wall": {
         "env_index": "all",
         "type": "box",
-        "scaling": f"{THICKNESS} {TABLE_Y_LEN} {HEIGHT}",
-        "transform": f"-{0.75*TABLE_X_LEN} 0 {0.5 * HEIGHT} 0 0 0 1",  # x y z qx qy qz qw
+        "scaling": f"{THICKNESS} {TABLE_Y_LEN} {MAX_HEIGHT}",
+        "transform": f"-{0.75*TABLE_X_LEN} 0 {0.5 * MAX_HEIGHT} 0 0 0 1",  # x y z qx qy qz qw
     },
     "front_wall": {
         "env_index": "all",
         "type": "box",
-        "scaling": f"{THICKNESS} {TABLE_Y_LEN} {HEIGHT}",
-        "transform": f"{TABLE_X_LEN} 0 {0.5 * HEIGHT} 0 0 0 1",  # x y z qx qy qz qw
+        "scaling": f"{THICKNESS} {TABLE_Y_LEN} {MAX_HEIGHT}",
+        "transform": f"{TABLE_X_LEN} 0 {0.5 * MAX_HEIGHT} 0 0 0 1",  # x y z qx qy qz qw
     },
     "table": {
         "env_index": "all",
@@ -70,12 +70,14 @@ world_dict_table_frame = {
         "env_index": "all",
         "type": "box",
         "scaling": f"{2*TABLE_X_LEN} {TABLE_Y_LEN} {THICKNESS}",
-        "transform": f"0 0 {HEIGHT} 0 0 0 1",  # x y z qx qy qz qw
+        "transform": f"0 0 {MAX_HEIGHT} 0 0 0 1",  # x y z qx qy qz qw
     },
 }
 
+SAFETY_BUFFER = 0.01  # MAKE THE TABLE HIGHER
+
 T_O_R = np.eye(4)
-T_O_R[:3, 3] = [-0.15, -0.2, -0.14]
+T_O_R[:3, 3] = [-0.15, -0.2, -0.165 + 0.5*THICKNESS - SAFETY_BUFFER]
 T_O_R[:3, :3] = R.from_euler("z", 36, degrees=True).as_matrix()
 
 T_R_O = np.linalg.inv(T_O_R)
